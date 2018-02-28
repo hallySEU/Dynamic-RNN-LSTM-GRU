7dd68f1017e126ae1f0807ae28d6fec26af71fd8
huangjunheng <huangjunheng@baidu.com>
2018-02-28 13:11:49 +0800

    add

---
+++ dynamic_sequence_model.py
@@ -0,0 +1,187 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+ Filename @ dynamic_sequence_model.py
+ Author @ huangjunheng
+ Create date @ 2018-02-26 15:09:27
+ Description @ rnn, lstm, gru integrate
+"""
+
+from __future__ import print_function
+
+import tensorflow as tf
+from tensorflow.contrib import rnn
+
+from model_config import ModelConfig
+from sequence_data import SequenceData
+from sequence_data import cal_model_para
+
+
+class DynamicSequenceModel:
+    """
+    整合 dynamic (rnn, lstm, gru) 功能
+    """
+    def __init__(self):
+        self.conf = ModelConfig()
+        self.seq_max_len, self.input_size, self.num_class = cal_model_para(filename=self.conf.training_data)
+        self._init_varible()
+        self.loss_op, self.optimizer_op, self.accuracy_op, self.predict_op = self.define_operator()
+
+    def _init_varible(self):
+
+        self.X = tf.placeholder(tf.float32, [None, self.seq_max_len, self.input_size])
+        self.Y = tf.placeholder(tf.float32, [None, self.num_class])
+
+        # A placeholder for indicating each sequence length
+        self.seqlen = tf.placeholder(tf.int32, [None])
+
+        self.biases = {
+            'out': tf.Variable(tf.random_normal([self.num_class]))
+        }
+        self.weights = {
+            'out': tf.Variable(tf.random_normal([self.conf.num_hidden, self.num_class]))
+        }
+
+    def cell_operation(self, X):
+        # 动态rnn提供了每个序列的长度信息，因为每个序列的长度不一样.
+        # 每一个sample的output取这个sample的最后长度的cell的输出作为隐层输出，即output[cur_len-1]，而不是静态rnn的output[-1]
+        # Prepare data shape to match `rnn` function requirements
+        # Current data input shape: (batch_size, n_steps, n_input)
+        # Required shape: 'n_steps' tensors list of shape (batch_size, n_input)
+
+        # Unstack to get a list of 'n_steps' tensors of shape (batch_size, n_input)
+        x = tf.unstack(X, self.seq_max_len, axis=1)
+
+        # Define a cell with tensorflow
+        if self.conf.model == 'rnn':
+            cell_func = rnn.BasicRNNCell
+        elif self.conf.model == 'lstm':
+            cell_func = rnn.BasicLSTMCell
+        elif self.conf.model == 'gru':
+            cell_func = rnn.GRUCell
+        else:
+            raise Exception("model type not supported: {}".format(self.conf.model))
+
+        print('Use %s model.' % self.conf.model)
+        # Define a lstm cell with tensorflow
+        cell = cell_func(self.conf.num_hidden)
+
+        # Get lstm cell output, providing 'sequence_length' will perform dynamic
+        # calculation.
+        outputs, states = tf.contrib.rnn.static_rnn(cell, x, dtype=tf.float32,
+                                                    sequence_length=self.seqlen)
+
+        # When performing dynamic calculation, we must retrieve the last
+        # dynamically computed output, i.e., if a sequence length is 10, we need
+        # to retrieve the 10th output.
+        # However TensorFlow doesn't support advanced indexing yet, so we build
+        # a custom op that for each sample in batch size, get its length and
+        # get the corresponding relevant output.
+
+        # 'outputs' is a list of output at every timestep, we pack them in a Tensor
+        # and change back dimension to [batch_size, n_step, n_hidden]
+        outputs = tf.stack(outputs)
+        outputs = tf.transpose(outputs, [1, 0, 2])
+
+        # Hack to build the indexing and retrieve the right output.
+        batch_size = tf.shape(outputs)[0]
+        # Start indices for each sample
+        # 获取按batch_size展开后，每一个sample的最后seq序列
+        # 例如：seqlen = [5, 7, 9, 11], seq_max_len = 20, 则 index = [4 26 48 70]
+        index = tf.range(0, batch_size) * self.seq_max_len + (self.seqlen - 1)
+        # Indexing
+        outputs = tf.gather(tf.reshape(outputs, [-1, self.conf.num_hidden]), index)
+
+        # Linear activation, using outputs computed above
+        return tf.matmul(outputs, self.weights['out']) + self.biases['out']
+
+    def define_operator(self):
+        """
+        定义算子
+        :return: 
+        """
+        logits = self.cell_operation(self.X)
+        y_ = tf.nn.softmax(logits)
+        predict_op = tf.argmax(y_, 1)
+        # Define loss and optimizer
+        loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=self.Y))
+        optimizer_op = tf.train.GradientDescentOptimizer(learning_rate=self.conf.learning_rate).minimize(loss_op)
+
+        # Evaluate model
+        correct_pred = tf.equal(tf.argmax(y_, 1), tf.argmax(self.Y, 1))
+        accuracy_op = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+        return [loss_op, optimizer_op, accuracy_op, predict_op]
+
+    def train(self, session):
+        """
+        模型训练函数
+        :param session: 
+        :return: 
+        """
+        training_set = SequenceData(filename=self.conf.training_data, max_seq_len=self.seq_max_len)
+        for step in range(1, self.conf.training_steps + 1):
+            batch_x, batch_y, batch_seqlen = training_set.next(self.conf.batch_size)
+            # Run optimization op (backprop)
+            session.run(self.optimizer_op,
+                        feed_dict={self.X: batch_x, self.Y: batch_y,
+                        self.seqlen: batch_seqlen})
+            if step % self.conf.display_steps == 0 or step == 1:
+                # Calculate batch accuracy & loss
+                acc, loss = session.run([self.accuracy_op, self.loss_op],
+                                        feed_dict={self.X: batch_x, self.Y: batch_y,
+                                                   self.seqlen: batch_seqlen})
+                print("Step " + str(step) + ", Minibatch Loss= " + \
+                      "{:.6f}".format(loss) + ", Training Accuracy= " + \
+                      "{:.5f}".format(acc))
+        print("Optimization Finished!")
+
+    def test(self, session):
+        """
+        测试函数
+        :param session: 
+        :return: 
+        """
+        test_set = SequenceData(filename=self.conf.test_data, max_seq_len=self.seq_max_len)
+        test_data = test_set.data
+        test_label = test_set.labels
+        test_seqlen = test_set.seqlen
+        print("Testing Accuracy:", \
+              session.run(self.accuracy_op, feed_dict={self.X: test_data, self.Y: test_label,
+                                                       self.seqlen: test_seqlen}))
+
+    def predict(self, session):
+        """
+        预测函数
+        :param session: 
+        :return: 
+        """
+        predict_set = SequenceData(filename=self.conf.predict_data, max_seq_len=self.seq_max_len)
+        predict_result = session.run(self.predict_op, feed_dict={self.X: predict_set.data,
+                                                                 self.seqlen: predict_set.seqlen})
+        predict_result_list = []
+        for predict_index in predict_result:
+            result = [0] * self.num_class
+            result[predict_index] = 1
+            predict_result_list.append(result)
+
+        # print("Predict Result:", predict_result)
+        print("Predict Result:", predict_result_list)
+
+    def main(self):
+        """
+        主函数
+        :return: 
+        """
+        with tf.Session() as session:
+            init = tf.global_variables_initializer()
+            session.run(init)
+
+            self.train(session)
+            self.test(session)
+            self.predict(session)
+
+
+if __name__ == '__main__':
+    model = DynamicSequenceModel()
+    model.main()
---
+++ sequence_data.py
@@ -0,0 +1,179 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+ Filename @ sequence_data.py
+ Author @ huangjunheng
+ Create date @ 2018-02-26 15:19:27
+ Description @ 
+"""
+import random
+import tensorflow as tf
+from tensorflow.contrib import rnn
+
+
+def cal_model_para(filename):
+    """
+    根据数据计算模型的参数
+    1. 最大sequence长度: max_seq_len
+    2. 单个输入特征的维度: input_size
+    3. label的维度，几分类就几个维度: num_class
+    :param filename: 
+    :return: 
+    """
+    max_seq_len = -1
+    fr = open(filename)
+    for i, line in enumerate(fr):
+        line = line.rstrip('\n')
+        data_split = line.split('&')
+        feature_data_list = data_split[0].split('\t')
+
+        if i == 0:
+            input_size = len(feature_data_list[0].split('#'))
+            num_class = len(data_split[1].split('\t'))
+
+        cur_seq_len = len(feature_data_list)
+        if cur_seq_len > max_seq_len:
+            max_seq_len = cur_seq_len
+
+    if max_seq_len % 10 != 0:
+        max_seq_len = ((max_seq_len / 10) + 1) * 10
+
+    print 'According to "%s", seq_max_len is set to %d, ' \
+          'input_size is set to %d, num_class is set to %d.' \
+          % (filename, max_seq_len, input_size, num_class)
+    return max_seq_len, input_size, num_class
+
+
+# ====================
+#  Sequence Data
+# ====================
+class SequenceData(object):
+    """ 
+    判断序列是随机的还是有顺序的，序列的长度是不定的
+    Generate or read sequence of data with dynamic length.
+    For example:
+    - Class 0: linear sequences (i.e. [0.1, 0.2, 0.3, 0.4,...])
+    - Class 1: random sequences (i.e. [0.23, 0.3, 0.1, 0.87,...])
+
+    NOTICE:
+    We have to pad each sequence to reach 'max_seq_len' for TensorFlow
+    consistency (we cannot feed a numpy array with inconsistent
+    dimensions). The dynamic calculation will then be perform thanks to
+    'seqlen' attribute that records every actual sequence length.
+    """
+
+    def __init__(self, filename, max_seq_len):
+        self.batch_id = 0
+        self.data, self.labels, self.seqlen = self.load_data(filename, max_seq_len)
+
+    def next(self, batch_size):
+        """ 
+        获取全量数据(长度为n_samples)中的批量数据(长度为batch_size)
+         e.g. n_samples = 100, batch_size = 16, batch_num = 7(6+1), last_batch_size = 4
+        Return a batch of data. When dataset end is reached, start over.
+        """
+        if self.batch_id == len(self.data):
+            self.batch_id = 0
+
+        batch_index = min(self.batch_id + batch_size, len(self.data))
+
+        batch_data = (self.data[self.batch_id: batch_index])
+        batch_labels = (self.labels[self.batch_id: batch_index])
+        batch_seqlen = (self.seqlen[self.batch_id: batch_index])
+
+        self.batch_id = batch_index
+        return batch_data, batch_labels, batch_seqlen
+
+    def cal_max_seq_len(self, filename):
+        """
+        计算最大sequence长度
+        :param filename: 
+        :return: 
+        """
+        max_seq_len = -1
+        fr = open(filename)
+        for line in fr:
+            line = line.rstrip('\n')
+            data_split = line.split('&')
+            feature_data_list = data_split[0].split('\t')
+            cur_seq_len = len(feature_data_list)
+            if cur_seq_len > max_seq_len:
+                max_seq_len = cur_seq_len
+
+        if max_seq_len % 10 != 0:
+            max_seq_len = ((max_seq_len / 10) + 1) * 10
+
+        return max_seq_len
+
+    def load_data(self, filename, max_seq_len=20):
+        """
+        加载数据
+        :return: 
+        """
+        fr = open(filename)
+        datas = []
+        labels = []
+        seqlen = []
+        for line in fr:
+            line = line.rstrip('\n')
+            data_split = line.split('&')
+            feature_data_list = data_split[0].split('\t')
+            cur_seq_len = len(feature_data_list)
+            seqlen.append(cur_seq_len)
+
+            input_size = len(feature_data_list[0].split('#'))
+            s = [[float(i) for i in item.split('#')] for item in feature_data_list]
+            s += [[0.] * input_size for i in range(max_seq_len - cur_seq_len)]
+            datas.append(s)
+
+            if len(data_split) > 1: # 区分训练与预测
+                label_data_list = data_split[1].split('\t')
+                labels.append([float(item) for item in label_data_list])
+
+        return datas, labels, seqlen
+
+    def _data_generator(self, n_samples=500, max_seq_len=20, min_seq_len=3,
+                        max_value=1000):
+        """
+        序列数据生成器
+        :return: 
+        """
+        for i in range(n_samples):
+            # Random sequence length
+            len = random.randint(min_seq_len, max_seq_len)
+            # Monitor sequence length for TensorFlow dynamic calculation
+            self.seqlen.append(len)
+            # Add a random or linear int sequence (50% prob)
+            if random.random() < .5:
+                # Generate a linear sequence
+                rand_start = random.randint(0, max_value - len)
+                s = [[float(i) / max_value] for i in
+                     range(rand_start, rand_start + len)]
+                # Pad sequence for dimension consistency
+                s += [[0.] for i in range(max_seq_len - len)]
+                self.data.append(s)
+                self.labels.append([1., 0.])
+            else:
+                # Generate a random sequence
+                s = [[float(random.randint(0, max_value)) / max_value]
+                     for i in range(len)]
+                # Pad sequence for dimension consistency
+                s += [[0.] for i in range(max_seq_len - len)]
+                self.data.append(s)
+                self.labels.append([0., 1.])
+
+    def test(self):
+        """
+        测试
+        :return: 
+        """
+        filename = 'data/test_data.txt'
+        max_seq_len = self.cal_max_seq_len(filename)
+        self.load_data(filename, max_seq_len)
+
+
+if __name__ == '__main__':
+    # s_data = SequenceData()
+    # s_data.test()
+    filename = 'data/test_data.txt'
+    cal_model_para(filename=filename)
